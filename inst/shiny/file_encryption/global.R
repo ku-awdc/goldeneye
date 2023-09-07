68e2e06e68f70bdbd888c84c086cf01035452dcd
Matt Denwood <md@sund.ku.dk>
2023-09-07 08:05:11 +0200

    Remove unneeded stuff in global.R

--- inst/shiny/file_encryption/global.R
+++ inst/shiny/file_encryption/global.R
@@ -3,6 +3,9 @@ packages <- c("goldeneye","shiny","shinythemes")
 
 library("goldeneye")
 library("keyring")
+library("tidyverse")
+library("shiny")
+library("shinythemes")
 
 ## Set up default user if possible:
 password_text <- "Please enter your password"
@@ -24,54 +27,3 @@ if(!is.null(getOption('goldeneye_path')) && file.exists(getOption('goldeneye_pat
     password_text <- "Password retrieved from keyring"
   }
 }
-
-
-# Needs to be explicitly in here as library etc calls for deployment to shinyapps.io:
-library("bayescount")
-library("shiny")
-library("shinythemes")
-library("rhandsontable")
-library("stringr")
-# TODO: handle this more nicely
-
-# Load the packages:
-if(!all(sapply(packages, require, character.only=TRUE))) stop("One or more required package missing")
-
-# Other global settings and options:
-options(stringsAsFactors=FALSE)
-
-### For testing:
-testing <- FALSE
-# testing <- TRUE
-###
-
-parasitology <- FALSE
-
-headscript <- "Hello"
-footeraddtext <- "Footer"
-
-library("markdown")
-colwidth <- 4L
-
-waavp_choices <- list(
-	`*SELECT*` = "INVALID",
-	Ruminants = list(`Cattle - Nematodes - All` = "cattle", `Sheep - Nematodes - All` = "sheep", `Goats - Nematodes - All` = "goats"),
-	Equine = list(`Cyathostomins - Macroclyclic Lactones` = "cyath_ml", `Cyathostomins - Benzimidazoles` = "cyath_bz", `Cyathostomins - Pyrantel` = "cyath_pyr", `Foals - Parascaris - All` = "foals"),
-	`Swine` = list(`Oesophagostomum - Benzimidazoles` = "pig_bz", `Oesophagostomum - Ivermectin` = "pig_ivm")
-)
-
-host_choices <- list(
-	`*SELECT*` = "INVALID",
-	Ruminants = list(`Cattle` = "cattle", `Sheep` = "sheep", `Goats` = "goats"),
-	Equine = list(`Horses (adults)` = "horse_adult", `Horses (foals)` = "Horses (foals)", `Donkeys (adults)` = "donk_adult", `Donkeys (foals)` = "donk_foal"),
-	`Swine` = "pigs",
-	`Other` = "other"
-)
-
-repnull <- function(x) rep(NA_integer_, if(is.null(x)) 0L else x)
-changed <- function(x,input,settings) any(sapply(x, function(y) !is.null(input[[y]]) && !identical(input[[y]],settings[[y]])))
-
-status_feedback <- c(
-  'Please enter data via the "Data" tab.<br>For help, see the "Instructions" tab.',
-	'Please enter valid parameters via the "Parameters" tab.<br>For help, see the "Instructions" tab.'
-  )
